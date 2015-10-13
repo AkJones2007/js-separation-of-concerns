0b28528fffae62e25059c81ab0e29c78a3bc9707
Jeffrey Horn <jeffh@generalassemb.ly>
2015-10-13 15:09:18 -0400

    Update boilerplate

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,32 @@
 'use strict';
 
-require('./example');
-
+/* jshint -W079 */
 var $ = require('jquery');
 
-$(document).ready(function () {
-  console.log('It works.');
+var MyApp = {};
+
+$(document).ready(function() {
+  var yourName = prompt('What is your name?');
+
+  $('#name').html(yourName);
+
+  var $thingList = $('#fav-list');
+
+  var $button = $('#new-thing-button');
+  $button.on('click', function(event) {
+    event.preventDefault();
+    MyApp.addToList($thingList);
+  });
 });
+
+// We can define things outside of the window.onload which are evaluated
+// only when called.
+MyApp.addToList = function(list) {
+  var $newLi = $('<li>');
+  var $newItemText = $('#new-thing');
+  $newLi.html($newItemText.val());
+  $newItemText.val('');
+  if ($newLi.html() !== '') {
+    list.append($newLi);
+  }
+};
